1870196cd2cf843ff8310a65e51c7522a25ae2f8
Nuri Nawawi <nurihefez@gmail.com>
2022-03-29 00:42:43 +0700

    update processor by trimming all commas and dots. adding some words.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 	require_once "text_converter.php";
-	require_once "text_loader.php";
+	//require_once "text_loader.php";
+	$text = file_get_contents("text.txt");
 	require_once "text_processor.php";
 
 	
--- text_processor.php
+++ text_processor.php
@@ -1,19 +1,52 @@
 <html lang="ar" dir="rtl">
 <head>
+<style media="screen">
+@font-face {
+  font-family: 'Arabic';
+  font-style: normal;
+	src:url('Traditional_Arabic.ttf') ;
+}
+body {
+	font-family: 'Arabic';
+	font-size: 20px;
+}
+
+</style>
 </head>
+<body>
+
 <?php
 $pre = explode(" ", $text);
+
 foreach ( $pre as $post) {
+	$post = str_replace(":", "" , $post);
+	$post = str_replace(";", "" , $post);
+	$post = str_replace(",", "" , $post);
+	$post = str_replace(".", "" , $post);
 	$outp = $post;
+
 	foreach ($libs as $key => $value) {
-		if ( strtolower($post) == $key ) {
+		if ( strtolower($outp) == $key ) {
 			$outp = $value;
 		}
-	}
+		if ( strtolower($outp) == "se".$key."nya" ) {
+			$outp = "سَا".$value."يَا";
+		}
+		if ( strtolower($outp) == $key."mu" ) {
+			$outp = $value."مُوْ";
+		}
+		if ( strtolower($outp) == "mem".$key."nya" ) {
+			$outp = "مَامْـ".$value."يَا";
+		}
+		if ( strtolower($outp) == "mem".$key."nya" ) {
+			$outp = "مَامْـ".$value."يَا";
+		}
 
+
+
+	}
 	echo " " . $outp;
 }
 ?>
-<body>
 </body>
 </html>
